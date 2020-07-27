f5d8a9330c4c9959fd6cc9bbe8157a196987c689
codwerk <louis_widdy@live.fr>
2020-07-27 10:37:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <AppBar></AppBar>
+  </div>
+</template>
+
+<script>
+import AppBar from './components/AppBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppBar
+  }
+}
+</script>
+
+<style>
+
+@import url('./assets/scss/index.scss');
+@import url('https://use.fontawesome.com/releases/v5.8.2/css/all.css');
+@import url('https://fonts.googleapis.com/css?family=Nunito&display=swap');
+
+#app {
+  font-family: 'Nunito', Arial, Helvetica, sans-serif;
+  font-size: 1rem;
+  color: #333;
+  width: 100%;
+  min-height: 100vh;
+  overflow: hidden;
+}
+</style>
